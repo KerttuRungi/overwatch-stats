abcc3897797941833f24a699280fb8597220f95a
KerttuRungi <kerttu.rungi@gmail.com>
2026-09-15 20:56:13 +0300

    Add database setup

---
+++ overwatch-stats/overwatch-backend/src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+from routes.get_player import router as player_router
+
+app = FastAPI()
+app.include_router(player_router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
